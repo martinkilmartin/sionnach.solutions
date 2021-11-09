85eb0a375eecf9390ec96978c0777cd2ce6fb9f1
sionnach <sionnach.solutions@gmail.com>
2021-11-09 16:40:32 -0500

    Removes debug outpu and moves botd

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -7,17 +7,15 @@ import { ALLOWED_COUNTRY, BLOCKED_COUNTRY } from '@constants/GEO_LOCK'
 export async function middleware(req: NextRequest): Promise<Response> {
   const { nextUrl: url, geo } = req
   const country = geo.country || 'XX'
-  console.error(`country:${country}`)
-  console.error(`allowed:${ALLOWED_COUNTRY.includes(country)}`)
+  const bot = await botdEdge(req, {
+    // The request id is excluded for demo purposes because
+    // Botd remembers your request id and will always show
+    // you the /bot-detected page if you're a bot, and
+    // never if you have been identified as a human
+    useRequestId: false,
+  })
+  if (bot && bot.status !== 200) return bot
   if (ALLOWED_COUNTRY.includes(country)) {
-    const bot = await botdEdge(req, {
-      // The request id is excluded for demo purposes because
-      // Botd remembers your request id and will always show
-      // you the /bot-detected page if you're a bot, and
-      // never if you have been identified as a human
-      useRequestId: false,
-    })
-    if (bot && bot.status !== 200) return bot
     const res = await ipRateLimit(req)
     if (res.status !== 200) return res
     url.searchParams.set('country', country)
