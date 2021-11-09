e409954ad347c2bdd2338fadf3d6cc0538ba6d5d
sionnach <sionnach.solutions@gmail.com>
2021-11-09 17:05:10 -0500

    Removes useRequestId

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -7,25 +7,19 @@ import { ALLOWED_COUNTRY, BLOCKED_COUNTRY } from '@constants/GEO_LOCK'
 export async function middleware(req: NextRequest): Promise<Response> {
   const { nextUrl: url, geo } = req
   const country = geo.country || 'XX'
-  const bot = await botdEdge(req, {
-    // The request id is excluded for demo purposes because
-    // Botd remembers your request id and will always show
-    // you the /bot-detected page if you're a bot, and
-    // never if you have been identified as a human
-    useRequestId: true,
-  })
-  if (bot && bot.status !== 200) {
+  const botRes = await botdEdge(req)
+  if (botRes && botRes.status !== 200) {
     return new Response('roblox', { status: 403 })
   } else if (ALLOWED_COUNTRY.includes(country)) {
-    const res = await ipRateLimit(req)
-    if (res.status !== 200) return res
+    const ipLimitRes = await ipRateLimit(req)
+    if (ipLimitRes.status !== 200) return ipLimitRes
     url.searchParams.set('country', country)
     url.searchParams.set('ip', getIP(req))
     return NextResponse.rewrite(url)
   } else if (BLOCKED_COUNTRY.includes(country)) {
     return new Response('Blocked for legal reasons', { status: 451 })
   } else {
-    return new Response(`Limited audience ${country}, bot ${bot}`, {
+    return new Response(`Limited audience ${country}, bot ${botRes}`, {
       status: 451,
     })
   }
